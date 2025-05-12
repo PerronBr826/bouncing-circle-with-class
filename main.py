8b08a38c37869db88cd9870d09d4acee5301d7b4
PerronBr826 <PerronBr826@northwested.org>
2025-05-12 10:56:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ def main():
 
     class Circle():
         """Round Class for Cirular Applications or something"""
-        def __init__(self, color=[155,255,155], size=50, position=[200,300], velocity=[2,2]):
+        def __init__(self, color=[155,255,155], radius=50, position=[200,300], velocity=[2,2]):
             self.color = color
-            self.size = size
+            self.radius = radius
             self.position = position
             self.velocity = velocity
 
@@ -33,14 +33,21 @@ def main():
             self.position[0] += self.velocity[0]
             self.position[1] += self.velocity[1]
 
-            draw.draw_circle(screen, {'color':self.color, 'position':self.position, 'radius':50})
+            # Bounce off the left and right walls
+            if self.position[0] <= self.radius or self.position[0] >= config.WINDOW_WIDTH - self.radius:
+                self.velocity[0] *= -1
+
+            # Bounce off the top and bottom walls
+            if self.position[1] <= self.radius or self.position[1] >= config.WINDOW_HEIGHT - self.radius:
+                self.velocity[1] *= -1
+
+            draw.draw_circle(screen, {'color':self.color, 'position':self.position, 'radius':self.radius})
 
     circles = []
 
     for i in range(random.randint(5,10)):
-        circles.append(Circle(color=[random.randint(1,255),random.randint(1,255),random.randint(1,255)], size=random.randint(5,20), position=[random.randint(0,200),random.randint(0,200)], velocity=[random.choice([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]), random.choice([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5])]))
+        circles.append(Circle(color=[random.randint(1,255),random.randint(1,255),random.randint(1,255)], radius=random.randint(5,20), position=[random.randint(30,200),random.randint(30,200)], velocity=[random.choice([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]), random.choice([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5])]))
         
-
 
     while running:
         for event in pygame.event.get():
