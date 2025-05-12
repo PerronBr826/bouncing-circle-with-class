c4538aef34aef178d929bb937f55d6cee0cd7164
PerronBr826 <PerronBr826@northwested.org>
2025-05-12 10:45:18 -0400

    cool

--- main.py
+++ main.py
@@ -16,9 +16,27 @@ def init_game ():
 
 
 def main():
+
     screen = init_game()
     clock = pygame.time.Clock() # Initialize the clock here
     running = True
+
+    class Circle():
+        """Round Class for Cirular Applications or something"""
+        def __init__(self, color=[155,255,155], size=50, position=[200,300], velocity=[2,2]):
+            self.color = color
+            self.size = size
+            self.position = position
+            self.velocity = velocity
+
+        def move(self):
+            self.position[0] += self.velocity[0]
+            self.position[1] += self.velocity[1]
+
+            draw.draw_circle(screen, {'color':self.color, 'position':self.position, 'radius':50})
+
+    ball = Circle()
+
 
     while running:
         for event in pygame.event.get():
@@ -30,6 +48,8 @@ def main():
 
         screen.fill(config.WHITE) # Use color from config
 
+
+        ball.move()
     
 
         pygame.display.flip()
